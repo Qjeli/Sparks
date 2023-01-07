9c0ada4dff2a929a3fb3d65140520f7b3edd211c
Nastya <nastya12.11@yandex.ru>
2023-01-07 14:11:34 +0300

    change arif

--- arifmet.h
+++ arifmet.h
@@ -1,30 +1,81 @@
-#ifndef SRC_DESIMAL_H_
-#define SRC_DESIMAL_H_
+//#ifndef arifmet
+//#define arifmet
 
-#include "stdio.h" 
-/*0 - OK
-1 - the number is too large or equal to infinity
-2 - the number is too small or equal to negative infinity
-3 - division by 0*/
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define BITS_SIZE 4
-typedef struct // структура для decimal
+
+typedef struct 
 {
     int bits[BITS_SIZE];
 } s21_decimal;
 
-typedef struct 
-{
-    int sign; // знак
-    int move; // сдвиг запятой числа
-} info;
+#define OK 0
+#define s21_START_INFO 96
+#define s21_NAN 3
+#define TOO_MUCH_OR_INF 1
+#define TOO_FEW_OR_NEG_INF  2
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_getBit(s21_decimal d, int i); //получения 1 или 0 для каждого бита
+void s21_setBit(s21_decimal d, int i, int value); // определенной позиции переменной 1
+int s21_getScale(const s21_decimal d); //Возвращает масштаб данных, разрешенных в этом свойстве
+void s21_setScale(s21_decimal d, int scale); //для увеличение или уменьшение масштаба
+int s21_getSign(const s21_decimal d); //заменa  параметра
+void s21_setSign( s21_decimal d, int sign);
 
-// доп функции
-void set_info(s21_decimal value, info *info_vall);
-int get_info(s21_decimal value, int i);
-#endif // SRC_DESIMAL_H_
+void s21_init_decimal(s21_decimal *dec);
+int s21_scale_equalization(s21_decimal *value_1, s21_decimal *value_2, int err_num);
+int s21_integer_division(s21_decimal value_1, s21_decimal value_2, s21_decimal *result, s21_decimal *remainder, int error_code);
+void s21_decl_to_null(s21_decimal *decl);
+
+void s21_setting(s21_decimal tmp_buf, s21_decimal *tmp_del, s21_decimal *tmp_mod, int *discharge);
+void s21_first_prepare(s21_decimal tmp_div, s21_decimal *tmp_mod,s21_decimal *tmp_del, s21_decimal value_2, int *discharge);
+
+
+int s21_last_bit(s21_decimal value);
+void s21_bits_copy(s21_decimal value, s21_decimal *dest);
+
+
+int s21_shift_left(s21_decimal *value, int num);
+int s21_shift_right(s21_decimal *first, int shift);
+
+void s21_decl_to_null(s21_decimal *decl);
+
+
+int s21_is_less(s21_decimal value1, s21_decimal value2); // Меньше
+int s21_is_less_or_equal(s21_decimal, s21_decimal); // Меньше или равно
+int s21_is_greater(s21_decimal value_1, s21_decimal value_2); // Больше
+int s21_is_greater_or_equal(s21_decimal value1, s21_decimal value2); // Больше или равно
+int s21_is_equal(s21_decimal value1, s21_decimal value2); // Равно
+int s21_is_not_equal(s21_decimal, s21_decimal); //Не равно
+
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //сложение
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //вычитание
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //умножение
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //деление
+int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //остаток от деления
+
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst);
+int s21_from_float_to_decimal(float src, s21_decimal *dst);
+int s21_from_decimal_to_int(s21_decimal src, int *dst);
+int s21_from_decimal_to_float(s21_decimal src, float *dst);
+
+//  bit[0] is small 32 bits
+//  bit[1] is medium 32 bits
+//  bit[2] is high 32 bits
+//  bit[3]
+//  Биты от 0 до 15, младшее слово, не используются и должны быть равны нулю.
+//  Биты с 16 по 23 должны содержать показатель степени от 0 до 28, который указывает степень 10 для разделения целого числа.
+//  Биты с 24 по 30 не используются и должны быть равны нулю.
+//  Бит 31 содержит знак; 0 означает положительный, а 1 означает отрицательный
+
+
+//#endif  // arifmet
