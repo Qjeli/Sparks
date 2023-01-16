559c9cb11682064ee515be17c51a1195cf5a3df2
Kayce Keytar <kaycekey@pr-h1.kzn.21-school.ru>
2023-01-16 22:51:07 +0300

    negation done but not tested

---
+++ anot_func.c
@@ -0,0 +1,11 @@
+#include "arifmet.h"
+
+int s21_negate(s21_decimal value, s21_decimal *result) {
+    for (int i = 0; i < 4; i++) {
+        result->bits[i] = value.bits[i];
+    }
+    s21_setSign(result, !s21_getSign(value));
+    return 0;
+}
+
+// не получилось протестировать пока ещё
