9944de2997af60842967cbdb195a8a7952da7dc0
Bulat <kaycekey@DESKTOP-RVN2BG7>
2023-02-09 22:24:05 +0300

    1/4 funcs testing successfully

--- s21_another.c
+++ s21_another.c
@@ -13,8 +13,7 @@ int s21_negate(s21_decimal value, s21_decimal *result) {
 
     s21_setSign(result, !s21_getSign(value));
     return 0;
-} // works, but the conversion seems odd
-// need to add error check?
+}
 
 // src: grishakir
 int s21_truncate(s21_decimal value, s21_decimal *result) {
